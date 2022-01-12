bbb3480c1b8852711c6a3ec1e86f10a2cd787fae
TS93 <tschram93@yahoo.com>
2022-01-11 23:13:33 -0500

    adding styles

---
+++ script.js
@@ -0,0 +1,11 @@
+// Date Variables
+var date = $("#currentDay");
+var time = moment().format("dddd, MMMM Mo");
+
+date.append(time);
+
+// Timeblocks
+
+// Event Listeners
+
+// Save Events to localstorage
